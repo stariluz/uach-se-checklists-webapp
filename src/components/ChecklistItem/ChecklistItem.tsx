293b5c69be1e9f9fe8f6149aaef2d55228270061
Adora <im.stariluz@gmail.com>
2024-10-29 22:02:16 -0600

    Advance #8 Checklist base item done

---
+++ src/components/ChecklistItem/ChecklistItem.tsx
@@ -0,0 +1,41 @@
+import ProfilePicture from '../UserImage/ProfilePicture';
+import './ChecklistItem.css'
+
+class ChecklistItemModel {
+    title?: string
+    user_id?: string
+    user?: any
+    created_at?: Date = new Date()
+    updated_at?: Date = new Date()
+    url?:string
+}
+interface Props {
+    className?: string
+    checklist?: ChecklistItemModel
+}
+interface Ownership {
+    user_id?: string
+    user?: any
+}
+const ChecklistOwnership = (props: Ownership) => {
+    return props.user_id ?
+        <span>de: {props.user.full_name || "User or fullname missing"}
+            <ProfilePicture
+                picture_url={props.user.picture_url}
+            />
+        </span>
+        :
+        <span className='ownership'>Compartido</span>
+}
+const ChecklistItem = (props: Props) => {
+    return <article className='item'>
+        <a className='item-link' href={props.checklist?.url || "#"}></a>
+        <h2 className='item-header'>{props.checklist?.title || "Title is missing"}</h2>
+        <div className='item-subheader'>
+            <ChecklistOwnership />
+            <span>Creado el {props.checklist?.created_at?.toString() || "dd de mmmm del yyyy"}</span>
+            <span>Ult. Modificación el {props.checklist?.updated_at?.toString() || "dd de mmmm del yyyy"}</span>
+        </div>
+    </article>
+}
+export default ChecklistItem;
--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -2,21 +2,15 @@ import reactLogo from '../assets/react.svg'
 import './HomePage.css'
 import ButtonsDev from '../components/Development/ButtonsDev'
 import Header from '../components/Header/Header'
+import ChecklistItem from '../components/ChecklistItem/ChecklistItem'
 
 function HomePage() {
   return (
     <>
       <Header/>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <ChecklistItem/>
+      <h1>SSLTC</h1>
       <ButtonsDev></ButtonsDev>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
