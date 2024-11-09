5c5ebba52ff1ae7e25c06f6d8d782ddd9b3374c5
Adora <im.stariluz@gmail.com>
2024-11-09 17:49:23 -0600

    Advance #14 TaskGroup models

--- src/models/TaskGroups/TaskGroupModel.ts
+++ src/models/TaskGroups/TaskGroupModel.ts
@@ -1,20 +1,18 @@
 import { randomUUID, UUID } from "crypto";
 import BaseModel from "../BaseModel";
-import ITaskModel from "./ITaskModel";
+import ITaskGroupModel from "./ITaskGroupModel";
 
-class TaskModel extends BaseModel implements ITaskModel {
+class TaskGroupModel extends BaseModel implements ITaskGroupModel {
     checklist_id: UUID = randomUUID();
-    task_group_id: UUID = randomUUID();
     title: string = "";
     due_date: Date = new Date();
     completed_at: Date = new Date();
-    constructor(data: ITaskModel = {}) {
+    constructor(data: ITaskGroupModel = {}) {
         super({ ...data });
         this.checklist_id = data.checklist_id ?? this.checklist_id;
-        this.task_group_id = data.task_group_id ?? this.task_group_id;
         this.title = data.title ?? this.title;
         this.due_date = data.due_date ?? this.due_date;
         this.completed_at = data.completed_at ?? this.completed_at;
     }
 }
-export default TaskModel;
+export default TaskGroupModel;
--- src/models/Users/UserModel.ts
+++ src/models/Users/UserModel.ts
@@ -5,11 +5,11 @@ class UserModel extends BaseModel implements IUserModel {
     google_token: string = '';
     email: string = '';
     picture_url: string = '';
-    constructor(task: IUserModel = {}) {
-        super({ ...task });
-        this.google_token = task.google_token ?? this.google_token;
-        this.email = task.email ?? this.email;
-        this.picture_url = task.picture_url ?? this.picture_url;
+    constructor(data: IUserModel = {}) {
+        super({ ...data });
+        this.google_token = data.google_token ?? this.google_token;
+        this.email = data.email ?? this.email;
+        this.picture_url = data.picture_url ?? this.picture_url;
     }
 }
 export default UserModel;
