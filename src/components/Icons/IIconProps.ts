e11a9904e34a29fab92add59acc118089c581d4d
Adora <im.stariluz@gmail.com>
2024-11-09 19:38:30 -0600

    Complete #16 icons from svgs and remove of library

---
+++ src/components/Icons/IIconProps.ts
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+
+interface IIconProps {
+    children?: ReactNode,
+    className?: string;
+}
+
+export default IIconProps;
---
+++ src/components/Icons/index.ts
@@ -0,0 +1,18 @@
+import Icon12Hours from "./Icon12Hours";
+import IconDoorExit from './IconDoorExit'
+import IconEdit from './IconEdit'
+import IconPencilPlus from './IconPencilPlus'
+import IconShare from './IconShare'
+import IconTrashXFilled from './IconTrashXFilled'
+import IconUserCircle from './IconUserCircle'
+
+export {
+    Icon12Hours,
+    IconDoorExit,
+    IconEdit,
+    IconPencilPlus,
+    IconShare,
+    IconTrashXFilled,
+    IconUserCircle,
+}
+
