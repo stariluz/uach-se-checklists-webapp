97f5c2991e3c29e68f800c4d7f85dbfa263bd360
Adora <im.stariluz@gmail.com>
2024-11-21 12:46:36 -0600

    #37 configuration for deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+if (!import.meta.env.VITE_GOOGLE_CLIENT_ID) {
+  console.error('VITE_GOOGLE_CLIENT_ID no está definido');
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
