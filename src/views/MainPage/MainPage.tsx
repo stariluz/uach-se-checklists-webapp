13769bfab29b057300fe98294677487b24f9cc54
MAbrahamEM <abraham-escudero@hotmail.com>
2024-11-21 08:55:58 -0600

    Design of Main Page

--- src/views/MainPage/MainPage.tsx
+++ src/views/MainPage/MainPage.tsx
@@ -19,15 +19,24 @@ function MainPage() {
   return (
     <div className="main-container">
       <div className="header">
+        <div className="title">¡Sé un Checkmylist!</div> {/* Título agregado */}
+        <div className="subtitle">
+          Tus pendientes, proyectos, tareas, listas de compras, quehaceres y más en un sitio, de forma sencilla, y colaborativa.
+        </div>
+        <div className='crea'>¡Crea tú cuenta!</div>
+        <div className='cuenta'>¿Ya tienes una cuenta?</div>
         <div className="auth-buttons">
-          <GoogleLoginLogic buttonText="signin_with" onLoginSuccess={handleLoginSuccess} />
           <GoogleSignupLogic buttonText="signup_with" onSignupSuccess={handleSignupSuccess} />
+          <GoogleLoginLogic buttonText="signin_with" onLoginSuccess={handleLoginSuccess} />
           <GoogleLogoutComponent />
         </div>
         {profilePicture && <img src={profilePicture} alt="Profile" className="profile-picture" />}
+      </div>
+      <div className="task-message">
+        ¡Crea tus listas de tareas y compártelas con quien tú quieras!
       </div>
     </div>
   );
 }
 
-export default MainPage;
+export default MainPage;  
